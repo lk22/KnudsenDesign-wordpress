3dbf0cdfe86666d9301a857126e0bd500e3338b1
Leo Knudsen <nightskater-220@hotmail.com>
2017-09-04 17:58:29 +0200

    add footer component

--- components/header.php
+++ components/header.php
@@ -1,10 +1,10 @@
 <!-- desktop header -->
 <header class="container-fluid app_header hidden-sm hidden-xs">
-	<div class="col-md-3 col-lg-3 brand">
+	<div class="col-md-4 col-lg-4 brand">
 		<img class="pull-left" src="http://leoknudsen.dev/wp-content/uploads/2017/09/14962721_319988798381318_775170380127919428_n.png" height="45" width="45" alt="">
 		<p class="navbar-brand"><?php bloginfo('name'); ?></p>
 	</div>
-	<div class="col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3 nav-wrapper">
+	<div class="col-md-5 col-md-offset-3 col-lg-5 col-lg-offset-3 nav-wrapper">
 		<!-- add navigation menu -->
 		<?php add_menu(); ?>
 	</div>
