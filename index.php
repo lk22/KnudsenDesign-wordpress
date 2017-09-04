fe73383c028af37221b7f9de8a3f535b90a111a7
Leo Knudsen <nightskater-220@hotmail.com>
2017-09-04 16:50:37 +0200

    setup content check (index.php)

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
 <?php 
 
-
+/**
+ * get header
+ */
 get_header();
+
+// check if content exists
+if(have_posts()) {
+
+	// having content posts
+	while(have_posts()){
+		// get the ppost
+		the_post();
+
+		// get content part
+		get_template_part('content', get_post_format());
+	}
+}
 
 
 get_footer();
-
-
-
-
-
 
  ?>
