b27ed4317ddc7156a0c931b0dc3dfc98fd9559af
Leo Knudsen <nightskater-220@hotmail.com>
2017-09-04 18:40:24 +0200

    start building home header section

--- content.php
+++ content.php
@@ -5,7 +5,9 @@
 
 			?> 
 
-				
+				<h2 class="text-center">Brug for et nyt website</h2>
+				<h4 class="text-center">eller måske brug for hjælp til en mindre opgave</h4>
+				<h5 class="text-center">Tag kontakt til mig <a href="/contact">her</a></h5>
 
 			<?php
 
